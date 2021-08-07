3abd4ad48ba304ceb94d20fe9e0c99e032701646
RevathySanthanam97 <revathysanthanam97@gmail.com>
2021-08-08 03:37:37 +0530

    updated

---
+++ src/Map.js
@@ -0,0 +1,8 @@
+import React from "react";
+// import { TileLayer } from "react-leaflet";
+// import { Map as LeafletMap } from "leaflet";
+function Map() {
+  return <div className="map"></div>;
+}
+
+export default Map;
